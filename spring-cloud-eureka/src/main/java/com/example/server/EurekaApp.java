593ede99ef2995ae5ca137bd3bacc52c1f9b2d4a
LL <wsliulei@163.com>
2018-06-04 16:56:52 +0800

    yml

--- spring-cloud-eureka/src/main/java/com/example/server/EurekaApp.java
+++ spring-cloud-eureka/src/main/java/com/example/server/EurekaApp.java
@@ -3,10 +3,18 @@ package com.example.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class EurekaApp {
+
+    @RequestMapping("/home")
+    public String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         
