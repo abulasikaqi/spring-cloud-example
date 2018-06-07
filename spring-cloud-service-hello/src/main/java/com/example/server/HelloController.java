b34123888133a429a63162032e0999752426c9a4
LL <wsliulei@163.com>
2018-06-07 16:06:04 +0800

    param

--- spring-cloud-service-hello/src/main/java/com/example/server/HelloController.java
+++ spring-cloud-service-hello/src/main/java/com/example/server/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,7 +25,7 @@ public class HelloController {
     private DiscoveryClient client;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String index() {
+    public String index(@RequestParam("name") String name) {
 
         List<ServiceInstance> instances = client.getInstances("hello-service");
         for (ServiceInstance instance : instances) {
@@ -32,6 +33,6 @@ public class HelloController {
             logger.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", port:" + instance.getPort());
         }
 
-        return "Hello World!";
+        return "Hello " + name + "!";
     }
 }
