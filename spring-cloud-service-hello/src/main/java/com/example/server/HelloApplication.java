070696822a31a38e30bfa710abdab0706f726f4f
LL <wsliulei@163.com>
2018-06-07 15:18:47 +0800

    @EnableEurekaClient change to @EnableDiscoveryClient

--- spring-cloud-service-hello/src/main/java/com/example/server/HelloApplication.java
+++ spring-cloud-service-hello/src/main/java/com/example/server/HelloApplication.java
@@ -2,13 +2,13 @@ package com.example.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 419
  */
+@EnableDiscoveryClient
 @SpringBootApplication
-@EnableEurekaClient
 public class HelloApplication {
 
     public static void main(String[] args) {
